9ae3a9245cc3823e4fa4c5d5bdfc1e1d1acf66cf
vinhactindi <nguyen.the.vinh@actindi.net>
2021-11-09 22:08:11 +0700

    bowling in object-oriented programming

---
+++ 07.bowling_object/frame.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require './shot'
+
+class Frame
+  attr_reader :first_shot, :second_shot, :third_shot
+
+  def initialize(first_mark, second_mark, third_mark = nil)
+    @first_shot = Shot.new(first_mark)
+    @second_shot = Shot.new(second_mark)
+    @third_shot = Shot.new(third_mark)
+  end
+
+  def score
+    [first_shot.score, second_shot.score, third_shot.score].sum
+  end
+
+  def strike?
+    first_shot.score == 10
+  end
+
+  def spare?
+    first_shot.score + second_shot.score == 10 && third_shot.mark.nil?
+  end
+end
---
+++ 07.bowling_object/game.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require './frame'
+
+class Game
+  attr_reader :frames
+
+  def initialize(score)
+    scores = score.split(',')
+    @frames = []
+
+    until scores.empty?
+      shifted_score = scores.shift
+
+      @frames << if @frames.size == 9
+                   Frame.new(shifted_score, scores.shift, scores.shift)
+                 elsif shifted_score == 'X'
+                   Frame.new(shifted_score, 0)
+                 else
+                   Frame.new(shifted_score, scores.shift)
+                 end
+    end
+  end
+
+  def score
+    point = 0
+    @frames.each_with_index do |frame, index|
+      point += point_and_bonus(frame, @frames[index + 1], @frames[index + 2])
+    end
+    point
+  end
+
+  private
+
+  def point_and_bonus(frame, next_frame, nnext_frame)
+    if frame.strike? && next_frame&.strike? && nnext_frame
+      20 + nnext_frame.first_shot.score
+    elsif frame.strike? && next_frame
+      10 + next_frame.first_shot.score + next_frame.second_shot.score
+    elsif frame.spare? && next_frame
+      10 + next_frame.first_shot.score
+    else
+      frame.score
+    end
+  end
+end
