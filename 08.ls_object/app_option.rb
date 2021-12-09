fa08e5061b62bf04d2bfe14a1eac56042fec7e1f
vinhactindi <nguyen.the.vinh@actindi.net>
2021-12-09 15:00:53 +0700

    handle dir and entry_paths in main

--- 08.ls_object/app_option.rb
+++ 08.ls_object/app_option.rb
@@ -14,22 +14,4 @@ class AppOption
   def extras
     ARGV
   end
-
-  def dir
-    if extras.empty?
-      yield(entry_paths('./'), './')
-    else
-      extras.each do |path|
-        yield(entry_paths(path), path)
-      end
-    end
-  end
-
-  def entry_paths(dir)
-    output = Dir.glob("#{dir}/*").sort
-    output = Dir.glob("#{dir}/*", File::FNM_DOTMATCH).sort if has?('a')
-    output.reverse! if has?('r')
-
-    output
-  end
 end
--- 08.ls_object/ls.rb
+++ 08.ls_object/ls.rb
@@ -5,16 +5,21 @@ require './entry_list'
 require './app_option'
 
 option = AppOption.new
+paths = option.extras.empty? ? ['./'] : option.extras
 
-option.dir do |entry_paths, dir_path|
-  unless File.exist?(dir_path)
-    puts "#{dir_path}: No such file or directory"
+paths.each do |path|
+  unless File.exist?(path)
+    puts "#{path}: No such file or directory"
     next
   end
 
+  entry_paths = Dir.glob("#{path}/*").sort
+  entry_paths = Dir.glob("#{path}/*", File::FNM_DOTMATCH).sort if option.has?('a')
+  entry_paths.reverse! if option.has?('r')
+
   entries = EntryList.new(entry_paths)
 
-  puts "#{dir_path}:" if option.extras.length > 1
+  puts "#{path}:" if option.extras.length > 1
   if option.has?('l')
     puts entries.to_s_with_stats
   else
